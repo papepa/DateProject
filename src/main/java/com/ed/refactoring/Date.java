8e9e18d73854fd98892b0c515069f02331b86c2f
Pablo <pablopp0599@gmail.com>
2025-03-16 14:33:57 +0100

    Third commit: JavaDoc comments added in code.

--- src/main/java/com/ed/refactoring/Date.java
+++ src/main/java/com/ed/refactoring/Date.java
@@ -11,23 +11,38 @@ public class Date {
 
     private int day, month, year;
 
+/**
+* Constructor for Date object.
+* @param day   the day of the month
+* @param month the month of the year
+* @param year  the year
+*/
     public Date(int day, int month, int year) {
         this.day = day;
         this.month = month;
         this.year = year;
     }
 
-    // Checks if given day is between 1 and 31
+/**
+* Checks if the given day is valid (between 1 and 31).
+* @return true if the day is valid, false if the day is not valid
+*/
     public boolean validateDay() {
         return day >= 1 && day <= 31;
     }
 
-    // Checks if given month is between 1 and 12
+/**
+* Checks if the given month is valid (between 1 and 12).
+* @return true if the month is valid, false it the month id not valid.
+*/
     public boolean validateMonth() {
         return month >= 1 && month <= 12;
     }
 
-    // Returns the number of days in the month
+/**
+* Returns the number of days in the given month, considering leap years.
+* @return the number of days in the month.
+*/
     public int numberOfDays() {
         return switch (month) {
             case 4, 6, 9, 11 -> 30;
@@ -36,13 +51,19 @@ public class Date {
         };
     }
 
-    // Determines if the year is a leap year
+/**
+* Determines if the given year is a leap year.
+* @return true if the year is a leap year, false if not
+*/
     private boolean isLeapYear() {
         return (year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0));
     }
 
-    // Validates if the date is correct
-    public boolean validate() {
+/**
+* Validates if the date is correct.
+* @return true if the date is valid, false if the date us not valid
+*/
+    public boolean validateDate() {
         if (!validateMonth() || !validateDay()) {
             return false;
         }
